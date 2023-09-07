688e72491c6636227f413d5ed839f9d7d3510528
Amanksh <amankushwaha933@gmail.com>
2023-09-07 14:23:41 +0530

    port

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+
+import { Navbar } from './components/Navbar'
+import { Home } from './components/Home'
+import { About } from './components/About'
+import Skills from './components/Skills'
+import { Work } from './components/Work'
+import Contact from './components/Contact'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+   <Navbar/>
+   <Home/>
+   <About/>
+   <Skills/>
+   <Work/>
+   <Contact/>
+   
+   </div>
+  );
+}
+
+export default App;
